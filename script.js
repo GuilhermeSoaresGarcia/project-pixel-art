7d889e8ab490daa8afdcf89168470e5c2145342a
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-02-15 16:42:57 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const h1Title = document.getElementById('title');
-
 function createDivColorPalette() {
   tagDiv = document.createElement('div');
-  h1Title.parentElement.appendChild(tagDiv);
+  document.body.appendChild(tagDiv);
   tagDiv.id = 'color-palette';
 }
 createDivColorPalette();
 
 function createColorsOfPalette() {
   palette = document.getElementById('color-palette');
-  arrayColors = ['red', 'green', 'blue', 'black',];
+  arrayColors = ['black', 'red', 'green', 'blue'];
   for (i = 0; i < arrayColors.length; i += 1) {
     tagDiv = document.createElement('div');
     palette.appendChild(tagDiv);
